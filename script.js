1f2a0b23318a550ee149da005e4b98533c93c63e
Hadi Dastangoo <h.dastangoo@gmail.com>
2023-07-15 14:01:40 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const file_url = "https://raw.githubusercontent.com/HadiDastangoo/BAIChatRTLStyle/main/style.css";
+const file_ref = document.createElement("link");
+file_ref.setAttribute("rel", "stylesheet");
+file_ref.setAttribute("type", "text/css");
+file_ref.setAttribute("href", file_url);
+
+// inject css file
+document.getElementsByTagName("head")[0].appendChild(file_ref);
